68cd9637b59d84a3447f415f1a0f10b8559d78d3
xiangling-liao <lemoncount95@gmail.com>
2017-11-10 10:39:00 -0500

    Update WeatherContract.java

--- Sunshine Weather Forecast app/app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
+++ Sunshine Weather Forecast app/app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.android.sunshine.data;
 
 import android.net.Uri;
@@ -26,12 +12,6 @@ import com.example.android.sunshine.utilities.SunshineDateUtils;
  */
 public class WeatherContract {
 
-    /*
-     * The "Content authority" is a name for the entire content provider, similar to the
-     * relationship between a domain name and its website. A convenient string to use for the
-     * content authority is the package name for the app, which is guaranteed to be unique on the
-     * Play Store.
-     */
     public static final String CONTENT_AUTHORITY = "com.example.android.sunshine";
 
     /*
@@ -40,20 +20,6 @@ public class WeatherContract {
      */
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
-    /*
-     * Possible paths that can be appended to BASE_CONTENT_URI to form valid URI's that Sunshine
-     * can handle. For instance,
-     *
-     *     content://com.example.android.sunshine/weather/
-     *     [           BASE_CONTENT_URI         ][ PATH_WEATHER ]
-     *
-     * is a valid path for looking at weather data.
-     *
-     *      content://com.example.android.sunshine/givemeroot/
-     *
-     * will fail, as the ContentProvider hasn't been given any information on what to do with
-     * "givemeroot". At least, let's hope not. Don't be that dev, reader. Don't be that dev.
-     */
     public static final String PATH_WEATHER = "weather";
 
     /* Inner class that defines the table contents of the weather table */
@@ -67,22 +33,6 @@ public class WeatherContract {
         /* Used internally as the name of our weather table. */
         public static final String TABLE_NAME = "weather";
 
-        /*
-         * The date column will store the UTC date that correlates to the local date for which
-         * each particular weather row represents. For example, if you live in the Eastern
-         * Standard Time (EST) time zone and you load weather data at 9:00 PM on September 23, 2016,
-         * the UTC time stamp for that particular time would be 1474678800000 in milliseconds.
-         * However, due to time zone offsets, it would already be September 24th, 2016 in the GMT
-         * time zone when it is 9:00 PM on the 23rd in the EST time zone. In this example, the date
-         * column would hold the date representing September 23rd at midnight in GMT time.
-         * (1474588800000)
-         *
-         * The reason we store GMT time and not local time is because it is best practice to have a
-         * "normalized", or standard when storing the date and adjust as necessary when
-         * displaying the date. Normalizing the date also allows us an easy way to convert to
-         * local time at midnight, as all we have to do is add a particular time zone's GMT
-         * offset to this date to get local time at midnight on the appropriate date.
-         */
         public static final String COLUMN_DATE = "date";
 
         /* Weather ID as returned by API, used to identify the icon to be used */
@@ -109,14 +59,7 @@ public class WeatherContract {
          */
         public static final String COLUMN_DEGREES = "degrees";
 
-        /**
-         * Builds a URI that adds the weather date to the end of the forecast content URI path.
-         * This is used to query details about a single weather entry by date. This is what we
-         * use for the detail view query. We assume a normalized date is passed to this method.
-         *
-         * @param date Normalized date in milliseconds
-         * @return Uri to query details about a single weather entry
-         */
+       
         public static Uri buildWeatherUriWithDate(long date) {
             return CONTENT_URI.buildUpon()
                     .appendPath(Long.toString(date))
