91532821beea7161458935c22e16f73087898be1
xiangling-liao <lemoncount95@gmail.com>
2017-11-10 10:37:45 -0500

    Update SunshinePreferences.java

--- Sunshine Weather Forecast app/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ Sunshine Weather Forecast app/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.android.sunshine.data;
 
 import android.content.Context;
@@ -117,15 +103,6 @@ public final class SunshinePreferences {
 
         double[] preferredCoordinates = new double[2];
 
-        /*
-         * This is a hack we have to resort to since you can't store doubles in SharedPreferences.
-         *
-         * Double.doubleToLongBits returns an integer corresponding to the bits of the given
-         * IEEE 754 double precision value.
-         *
-         * Double.longBitsToDouble does the opposite, converting a long (that represents a double)
-         * into the double itself.
-         */
         preferredCoordinates[0] = Double
                  .longBitsToDouble(sp.getLong(PREF_COORD_LAT, Double.doubleToRawLongBits(0.0)));
         preferredCoordinates[1] = Double
@@ -162,23 +139,13 @@ public final class SunshinePreferences {
      * @param context Used to access SharedPreferences
      * @return true if the user prefers to see notifications, false otherwise
      */
-    public static boolean areNotificationsEnabled(Context context) {
-        /* Key for accessing the preference for showing notifications */
+    public static boolean areNotificationsEnabled(Context context) 
         String displayNotificationsKey = context.getString(R.string.pref_enable_notifications_key);
-
-        /*
-         * In Sunshine, the user has the ability to say whether she would like notifications
-         * enabled or not. If no preference has been chosen, we want to be able to determine
-         * whether or not to show them. To do this, we reference a bool stored in bools.xml.
-         */
         boolean shouldDisplayNotificationsByDefault = context
                 .getResources()
                 .getBoolean(R.bool.show_notifications_by_default);
 
-        /* As usual, we use the default SharedPreferences to access the user's preferences */
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-
-        /* If a value is stored with the key, we extract it here. If not, use a default. */
         boolean shouldDisplayNotifications = sp
                 .getBoolean(displayNotificationsKey, shouldDisplayNotificationsByDefault);
 
@@ -194,20 +161,7 @@ public final class SunshinePreferences {
     public static long getLastNotificationTimeInMillis(Context context) {
         /* Key for accessing the time at which Sunshine last displayed a notification */
         String lastNotificationKey = context.getString(R.string.pref_last_notification);
-
-        /* As usual, we use the default SharedPreferences to access the user's preferences */
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-
-        /*
-         * Here, we retrieve the time in milliseconds when the last notification was shown. If
-         * SharedPreferences doesn't have a value for lastNotificationKey, we return 0. The reason
-         * we return 0 is because we compare the value returned from this method to the current
-         * system time. If the difference between the last notification time and the current time
-         * is greater than one day, we will show a notification again. When we compare the two
-         * values, we subtract the last notification time from the current system time. If the
-         * time of the last notification was 0, the difference will always be greater than the
-         * number of milliseconds in a day and we will show another notification.
-         */
         long lastNotificationTime = sp.getLong(lastNotificationKey, 0);
 
         return lastNotificationTime;
