77d83d96ecc60913d327c912148567b6b43af802
xiangling-liao <lemoncount95@gmail.com>
2017-11-10 10:58:01 -0500

    Update ForecastAdapter.java

--- Sunshine Weather Forecast app/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ Sunshine Weather Forecast app/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.android.sunshine;
 
 import android.content.Context;
@@ -28,24 +14,15 @@ import android.widget.TextView;
 import com.example.android.sunshine.utilities.SunshineDateUtils;
 import com.example.android.sunshine.utilities.SunshineWeatherUtils;
 
-/**
- * {@link ForecastAdapter} exposes a list of weather forecasts
- * from a {@link android.database.Cursor} to a {@link android.support.v7.widget.RecyclerView}.
- */
+
 class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
 
     private static final int VIEW_TYPE_TODAY = 0;
     private static final int VIEW_TYPE_FUTURE_DAY = 1;
 
-    /* The context we use to utility methods, app resources and layout inflaters */
+   
     private final Context mContext;
 
-    /*
-     * Below, we've defined an interface to handle clicks on items within this Adapter. In the
-     * constructor of our ForecastAdapter, we receive an instance of a class that has implemented
-     * said interface. We store that instance in this variable to call the onClick method whenever
-     * an item is clicked in the list.
-     */
     final private ForecastAdapterOnClickHandler mClickHandler;
 
     /**
@@ -55,12 +32,6 @@ class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapt
         void onClick(long date);
     }
 
-    /*
-     * Flag to determine if we want to use a separate view for the list item that represents
-     * today. This flag will be true when the phone is in portrait mode and false when the phone
-     * is in landscape. This flag will be set in the constructor of the adapter by accessing
-     * boolean resources.
-     */
     private boolean mUseTodayLayout;
 
     private Cursor mCursor;
@@ -119,10 +90,7 @@ class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapt
 
     /**
      * OnBindViewHolder is called by the RecyclerView to display the data at the specified
-     * position. In this method, we update the contents of the ViewHolder to display the weather
-     * details for this particular position, using the "position" argument that is conveniently
-     * passed into us.
-     *
+     * position.
      * @param forecastAdapterViewHolder The ViewHolder which should be updated to represent the
      *                                  contents of the item at the given position in the data set.
      * @param position                  The position of the item within the adapter's data set.
@@ -160,40 +128,33 @@ class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapt
         /****************
          * Weather Date *
          ****************/
-         /* Read date from the cursor */
+      
         long dateInMillis = mCursor.getLong(MainActivity.INDEX_WEATHER_DATE);
-         /* Get human readable string using our utility method */
+        
         String dateString = SunshineDateUtils.getFriendlyDateString(mContext, dateInMillis, false);
 
-         /* Display friendly date string */
         forecastAdapterViewHolder.dateView.setText(dateString);
 
         /***********************
          * Weather Description *
          ***********************/
         String description = SunshineWeatherUtils.getStringForWeatherCondition(mContext, weatherId);
-         /* Create the accessibility (a11y) String from the weather description */
+    
         String descriptionA11y = mContext.getString(R.string.a11y_forecast, description);
 
-         /* Set the text and content description (for accessibility purposes) */
         forecastAdapterViewHolder.descriptionView.setText(description);
         forecastAdapterViewHolder.descriptionView.setContentDescription(descriptionA11y);
 
         /**************************
          * High (max) temperature *
          **************************/
-         /* Read high temperature from the cursor (in degrees celsius) */
+    
         double highInCelsius = mCursor.getDouble(MainActivity.INDEX_WEATHER_MAX_TEMP);
-         /*
-          * If the user's preference for weather is fahrenheit, formatTemperature will convert
-          * the temperature. This method will also append either °C or °F to the temperature
-          * String.
-          */
+        
         String highString = SunshineWeatherUtils.formatTemperature(mContext, highInCelsius);
-         /* Create the accessibility (a11y) String from the weather description */
+      
         String highA11y = mContext.getString(R.string.a11y_high_temp, highString);
 
-         /* Set the text and content description (for accessibility purposes) */
         forecastAdapterViewHolder.highTempView.setText(highString);
         forecastAdapterViewHolder.highTempView.setContentDescription(highA11y);
 
@@ -229,11 +190,7 @@ class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapt
 
     /**
      * Returns an integer code related to the type of View we want the ViewHolder to be at a given
-     * position. This method is useful when we want to use different layouts for different items
-     * depending on their position. In Sunshine, we take advantage of this method to provide a
-     * different layout for the "today" layout. The "today" layout is only shown in portrait mode
-     * with the first item in the list.
-     *
+     * position. 
      * @param position index within our RecyclerView and Cursor
      * @return the view type (today or future day)
      */
@@ -247,11 +204,7 @@ class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapt
     }
 
     /**
-     * Swaps the cursor used by the ForecastAdapter for its weather data. This method is called by
-     * MainActivity after a load has finished, as well as when the Loader responsible for loading
-     * the weather data is reset. When this method is called, we assume we have a completely new
-     * set of data, so we call notifyDataSetChanged to tell the RecyclerView to update.
-     *
+     * Swaps the cursor used by the ForecastAdapter for its weather data. 
      * @param newCursor the new cursor to use as ForecastAdapter's data source
      */
     void swapCursor(Cursor newCursor) {
@@ -259,11 +212,7 @@ class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapt
         notifyDataSetChanged();
     }
 
-    /**
-     * A ViewHolder is a required part of the pattern for RecyclerViews. It mostly behaves as
-     * a cache of the child views for a forecast item. It's also a convenient place to set an
-     * OnClickListener, since it has access to the adapter and the views.
-     */
+  
     class ForecastAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         final ImageView iconView;
 
@@ -285,10 +234,7 @@ class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapt
         }
 
         /**
-         * This gets called by the child views during a click. We fetch the date that has been
-         * selected, and then call the onClick handler registered with this adapter, passing that
-         * date.
-         *
+         * This gets called by the child views during a click. 
          * @param v the View that was clicked
          */
         @Override
