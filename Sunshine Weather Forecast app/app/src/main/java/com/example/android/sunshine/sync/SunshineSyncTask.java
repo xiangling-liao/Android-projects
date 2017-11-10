836acba11c2054c292d8b8231841e3e46b1a64b3
xiangling-liao <lemoncount95@gmail.com>
2017-11-10 10:44:47 -0500

    Update SunshineSyncTask.java

--- Sunshine Weather Forecast app/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ Sunshine Weather Forecast app/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.android.sunshine.sync;
 
 import android.content.ContentResolver;
@@ -41,11 +27,7 @@ public class SunshineSyncTask {
     synchronized public static void syncWeather(Context context) {
 
         try {
-            /*
-             * The getUrl method will return the URL that we need to get the forecast JSON for the
-             * weather. It will decide whether to create a URL based off of the latitude and
-             * longitude or off of a simple location as a String.
-             */
+           
             URL weatherRequestUrl = NetworkUtils.getUrl(context);
 
             /* Use the URL to retrieve the JSON */
@@ -55,12 +37,7 @@ public class SunshineSyncTask {
             ContentValues[] weatherValues = OpenWeatherJsonUtils
                     .getWeatherContentValuesFromJson(context, jsonWeatherResponse);
 
-            /*
-             * In cases where our JSON contained an error code, getWeatherContentValuesFromJson
-             * would have returned null. We need to check for those cases here to prevent any
-             * NullPointerExceptions being thrown. We also have no reason to insert fresh data if
-             * there isn't any to insert.
-             */
+           
             if (weatherValues != null && weatherValues.length != 0) {
                 /* Get a handle on the ContentResolver to delete and insert data */
                 ContentResolver sunshineContentResolver = context.getContentResolver();
@@ -76,17 +53,8 @@ public class SunshineSyncTask {
                         WeatherContract.WeatherEntry.CONTENT_URI,
                         weatherValues);
 
-                /*
-                 * Finally, after we insert data into the ContentProvider, determine whether or not
-                 * we should notify the user that the weather has been refreshed.
-                 */
                 boolean notificationsEnabled = SunshinePreferences.areNotificationsEnabled(context);
 
-                /*
-                 * If the last notification was shown was more than 1 day ago, we want to send
-                 * another notification to the user that the weather has been updated. Remember,
-                 * it's important that you shouldn't spam your users with notifications.
-                 */
                 long timeSinceLastNotification = SunshinePreferences
                         .getEllapsedTimeSinceLastNotification(context);
 
@@ -96,10 +64,7 @@ public class SunshineSyncTask {
                     oneDayPassedSinceLastNotification = true;
                 }
 
-                /*
-                 * We only want to show the notification if the user wants them shown and we
-                 * haven't shown a notification in the past day.
-                 */
+               
                 if (notificationsEnabled && oneDayPassedSinceLastNotification) {
                     NotificationUtils.notifyUserOfNewWeather(context);
                 }
