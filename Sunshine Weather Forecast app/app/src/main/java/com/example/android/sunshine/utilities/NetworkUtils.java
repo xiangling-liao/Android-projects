c1506a707549ccce804eb498b771d97fd3d562b1
xiangling-liao <lemoncount95@gmail.com>
2017-11-10 10:48:19 -0500

    Update NetworkUtils.java

--- Sunshine Weather Forecast app/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
+++ Sunshine Weather Forecast app/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.android.sunshine.utilities;
 
 import android.content.Context;
@@ -35,19 +21,6 @@ public final class NetworkUtils {
 
     private static final String TAG = NetworkUtils.class.getSimpleName();
 
-    /*
-     * Sunshine was originally built to use OpenWeatherMap's API. However, we wanted to provide
-     * a way to much more easily test the app and provide more varied weather data. After all, in
-     * Mountain View (Google's HQ), it gets very boring looking at a forecast of perfectly clear
-     * skies at 75°F every day... (UGH!) The solution we came up with was to host our own fake
-     * weather server. With this server, there are two URL's you can use. The first (and default)
-     * URL will return dynamic weather data. Each time the app refreshes, you will get different,
-     * completely random weather data. This is incredibly useful for testing the robustness of your
-     * application, as different weather JSON will provide edge cases for some of your methods.
-     *
-     * If you'd prefer to test with the weather data that you will see in the videos on Udacity,
-     * you can do so by setting the FORECAST_BASE_URL to STATIC_WEATHER_URL below.
-     */
     private static final String DYNAMIC_WEATHER_URL =
             "https://andfun-weather.udacity.com/weather";
 
@@ -55,13 +28,6 @@ public final class NetworkUtils {
             "https://andfun-weather.udacity.com/staticweather";
 
     private static final String FORECAST_BASE_URL = STATIC_WEATHER_URL;
-
-    /*
-     * NOTE: These values only effect responses from OpenWeatherMap, NOT from the fake weather
-     * server. They are simply here to allow us to teach you how to build a URL if you were to use
-     * a real API.If you want to connect your app to OpenWeatherMap's API, feel free to! However,
-     * we are not going to show you how to do so in this course.
-     */
 
     /* The format we want our API to return */
     private static final String format = "json";
@@ -84,16 +50,8 @@ public final class NetworkUtils {
     private static final String DAYS_PARAM = "cnt";
 
     /**
-     * Retrieves the proper URL to query for the weather data. The reason for both this method as
-     * well as {@link #buildUrlWithLocationQuery(String)} is two fold.
-     * <p>
-     * 1) You should be able to just use one method when you need to create the URL within the
-     * app instead of calling both methods.
-     * 2) Later in Sunshine, you are going to add an alternate method of allowing the user
-     * to select their preferred location. Once you do so, there will be another way to form
-     * the URL using a latitude and longitude rather than just a location String. This method
-     * will "decide" which URL to build and return it.
-     *
+     * Retrieves the proper URL to query for the weather data. 
+     * 
      * @param context used to access other Utility methods
      * @return URL to query weather service
      */
